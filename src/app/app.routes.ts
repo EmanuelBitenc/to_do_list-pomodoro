a7d45e21389935d786b713b5530197d16b514cc3
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-11-10 01:53:35 -0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './screens/home/home';
+import { Pomodoro } from './screens/pomodoro/pomodoro';
+import { Todo } from './screens/todo/todo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  {
+    path: 'todo',
+    component: Todo,
+  },
+  { path: 'pomodoro', component: Pomodoro },
+];
